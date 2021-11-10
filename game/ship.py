7abe7fce50d0d32ffbf088a0920de80b816c81e7
SolOFake <linkfazana2@gmail.com>
2021-11-10 11:09:32 +0200

    Игруха 10.11.2021

---
+++ game/ship.py
@@ -0,0 +1,18 @@
+import pygame
+class Ship:
+    """Класс для управления кораблем."""
+
+    def __init__(self, ai_game):
+        """Инициализирует корабль и задает его начальную позицию."""
+        self.screen = ai_game.screen
+        self.screen_rect = ai_game.screen.get_rect()
+        # Загружает изображение корабля и получает прямоугольник.
+        self.image = pygame.image.load('ship.bmp')
+        self.image = pygame.transform.scale(self.image, (100, 100))
+        self.rect = self.image.get_rect()
+        # Каждый новый корабль появляется у нижнего края экрана.
+        self.rect.midbottom = self.screen_rect.midbottom
+
+    def blitme(self):
+        """Рисует корабль в текущей позиции."""
+        self.screen.blit(self.image, self.rect)
