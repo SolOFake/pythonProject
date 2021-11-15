ac8657eaed3365db610ace6edced415b3db9fdc3
SolOFake <linkfazana2@gmail.com>
2021-11-15 16:43:48 +0200

    Визуализация данных

---
+++ csv_lesson/death_valley_highs_lows.py
@@ -0,0 +1,26 @@
+import csv
+
+filename = 'data/death_valley_2018_simple.csv'
+with open(filename) as f:
+    reader = csv.reader(f)
+    header_row = next(reader)
+
+    for index, column_header in enumerate(header_row):
+        print(index, column_header)
+
+    for row in reader:
+        current_date = datetime.strptime(row[2], '%Y-%m-%d')
+        try:
+            high = int(row[4])
+            low = int(row[5])
+        except ValueError:
+            print(f"Missing data for {current_date}")
+        else:
+            dates.append(current_date)
+            highs.append(high)
+            lows.append(low)
+
+# Форматирование диаграммы
+title = "Daily high and low temperatures - 2018\nDeath Valley, CA"
+plt.title(title, fontsize=20)
+plt.xlabel('', fontsize=16)
