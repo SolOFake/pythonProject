843cc1da9034d078d9aac63e41562b25804b928a
SolOFake <linkfazana2@gmail.com>
2021-11-29 02:45:07 +0200

    ататта

--- json_lesson/eq_explore_data.py
+++ json_lesson/eq_explore_data.py
@@ -7,4 +7,21 @@ with open(filename) as f:
 
 readable_file = 'data/readable_eq_data.json'
 with open(readable_file, 'w') as f:
-    json.dump(all_eq_data, f, indent=4)
+    json.dump(all_eq_data, f, indent=4)  # indent задает отступ
+
+all_eq_dicts = all_eq_data['features']  # КЛЮЧ
+print(len(all_eq_dicts))  # показывает количество данных по КЛЮЧ
+
+mags, lons, lats = [], [], []  # магнитуда, долгота, ширина
+
+for eq_dict in all_eq_dicts:
+    mag = eq_dict['properties']['mag']  # вытаскиваем значения по ключу
+    lon = eq_dict['geometry']['coordinates'][0]
+    lat = eq_dict['geometry']['coordinates'][1]
+    mags.append(mag)
+    lons.append(lon)  # Присоединяет к списку каждое значение
+    lats.append(lat)
+
+print(mags[:10])  # принтуем первые 10 значений
+print(lons[:5])
+print(lats[:5])
